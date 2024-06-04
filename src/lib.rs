530113d1c624a4c43a34745692ea09fc99817845
smndtrl <smndtrl@users.noreply.github.com>
2024-06-04 12:37:15 +0000

    use spin wit

--- src/lib.rs
+++ src/lib.rs
@@ -382,22 +382,22 @@ if_wasm! {
 
 
 if_wasi_http! {
-    pub mod wit {
-        wit_bindgen::generate!({
-            path: "wit",
-            world: "platform",
-            with: {
-                "wasi:io/error@0.2.0": spin_executor::bindings::wasi::io::error,
-                "wasi:io/streams@0.2.0": spin_executor::bindings::wasi::io::streams,
-                "wasi:io/poll@0.2.0": spin_executor::bindings::wasi::io::poll,
-            }
-        });
+    // pub mod wit {
+    //     wit_bindgen::generate!({
+    //         path: "wit",
+    //         world: "platform",
+    //         with: {
+    //             "wasi:io/error@0.2.0": spin_executor::bindings::wasi::io::error,
+    //             "wasi:io/streams@0.2.0": spin_executor::bindings::wasi::io::streams,
+    //             "wasi:io/poll@0.2.0": spin_executor::bindings::wasi::io::poll,
+    //         }
+    //     });
     
-        // pub struct Component;
+    //     // pub struct Component;
     
       
         
-    }
+    // }
     // #[cfg(test)]
     // #[macro_use]
     // extern crate doc_comment;
