fb65f64c20d1048670e3d9a31dcb00ec562a0a3c
smndtrl <smndtrl@users.noreply.github.com>
2024-11-07 14:42:04 +0000

    add `p1` to wasi target in case of adapter use

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -1,9 +1,9 @@
-#[cfg(all(target_os = "wasi", target_env = "p2"))]
+#[cfg(all(target_os = "wasi", any(target_env = "p1", target_env = "p2")))]
 pub mod component;
-#[cfg(all(target_os = "wasi", target_env = "p2"))]
+#[cfg(all(target_os = "wasi", any(target_env = "p1", target_env = "p2")))]
 pub use component::*;
 
-#[cfg(not(all(target_os = "wasi", target_env = "p2")))]
+#[cfg(not(all(target_os = "wasi", any(target_env = "p1", target_env = "p2"))))]
 pub mod js;
-#[cfg(not(all(target_os = "wasi", target_env = "p2")))]
+#[cfg(not(all(target_os = "wasi", any(target_env = "p1", target_env = "p2"))))]
 pub use js::*;
